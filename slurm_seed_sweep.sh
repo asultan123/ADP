e2f9977e4a9224b59ad3ca76738362f35f16418f
asultan123 <asultan@aucegypt.edu>
2022-03-29 21:59:26 -0400

    updated slurm seed sweep

--- slurm_seed_sweep.sh
+++ slurm_seed_sweep.sh
@@ -10,11 +10,17 @@
 #SBATCH --partition=gpu
 
 nvidia-smi
-conda activate tf
-cd /home/sultan.a/Adaptive-Diversity-Promoting
+# conda activate tf
+# cd /home/sultan.a/Adaptive-Diversity-Promoting
 
 seeds=(8388 1759 5933 7916 7445 6130 7422 4066 3098 5469 4456 2302 9062 2724 8420);
 
-for seed in ${seeds[@]}; do
-    echo $seed; 
-done
+function echo_seeds()
+{
+    for seed in ${seeds[@]}; do
+        echo $seed; 
+    done
+}
+
+echo_seeds;
+exit;
