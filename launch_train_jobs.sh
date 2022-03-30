6e88962cff9ca5a6168d849a2adee024d4723eca
asultan123 <asultan@aucegypt.edu>
2022-03-29 22:44:29 -0400

    updated sweep to multiple jobs

---
+++ launch_train_jobs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function train_seeds()
+{
+    seeds=(8388 1759 5933 7916 7445 6130 7422 4066 3098 5469 4456 2302 9062 2724 8420);
+    for seed in ${seeds[@]}; do
+        sbatch train_with_seed.sh $seed
+    done
+}
+
+train_seeds;
+
+exit;
+ 
--- train_with_seed.sh
+++ train_with_seed.sh
@@ -10,18 +10,11 @@
 #SBATCH --partition=gpu
 
 nvidia-smi
-seeds=(8388 1759 5933 7916 7445); #6130 7422 4066 3098 5469 4456 2302 9062 2724 8420);
 model_dir=/scratch/sultan.a/adp_models
 
 which python
 
-function train_seeds()
-{
-    for seed in ${seeds[@]}; do
-        python train_cifar.py --model_dir=$model_dir --seed=$seed --lamda=2 --log_det_lamda=0.5 --num_models=2 --augmentation=True --dataset=cifar10
-    done
-}
-
-train_seeds;
+seed=$1
+python train_cifar.py --model_dir=$model_dir --seed=$seed --lamda=2 --log_det_lamda=0.5 --num_models=2 --augmentation=True --dataset=cifar10
 
 exit;
