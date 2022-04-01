23e7bd60f52dda2babf1c1aa210b57599db6170a
asultan123 <asultan@aucegypt.edu>
2022-04-01 16:47:42 -0400

    updated launch scripts

--- train_with_seed.sh
+++ train_with_seed.sh
@@ -15,6 +15,7 @@ model_dir=/scratch/sultan.a/adp_models
 which python
 
 seed=$1
+echo Training with $seed
 python train_cifar.py --model_dir=$model_dir --seed=$seed --lamda=2 --log_det_lamda=0.5 --num_models=2 --augmentation=True --dataset=cifar10
 
 exit;
